4936751b0354c5252a945c9049ec0aeb50e559aa
shaneVR097 <121772441+shaneVR097@users.noreply.github.com>
2024-01-31 23:48:52 +0530

    Update ShySwitch1.java

--- ShySwitch1.java
+++ ShySwitch1.java
@@ -1,19 +1,19 @@
-public class ShySwitch1{
-public static void main(String args[]){
-int scr=49;
-switch(scr){
-case 90:
-System.out.println("Very Good");
-break;
-case 60:
-System.out.println("Good");
-break;
-case 40:
-System.out.println("Okay");
-break;
-default:
-System.out.println("Grade not defined");
-break;
-}
-}
+public class ShySwitch1 {
+    public static void main(String args[]) {
+        int scr = 49;
+        switch (scr) {
+            case 90:
+                System.out.println("Very Good");
+                break;
+            case 60:
+                System.out.println("Good");
+                break;
+            case 40:
+                System.out.println("Okay");
+                break;
+            default:
+                System.out.println("Grade not defined");
+                break;
+        }
+    }
 }
