62b2dc998e79c0952f1e48c616ca139680d650f7
shaneVR097 <121772441+shaneVR097@users.noreply.github.com>
2024-01-31 22:07:24 +0530

    Update ShyArr2.java

--- ShyArr2.java
+++ ShyArr2.java
@@ -1,18 +1,16 @@
-public class ShyArr2{
-public static void main(String args[]){
-int[] myArr={100,80,60,40,20};
-/*
-int[] myArr=new int[3];
-int[] myArr={1,2,3};
-int[] myArr=new int[]{1,2,3};
-*/
-for(int i=0;i<5;i++)
+public class ShyArr2
 {
-System.out.println(myArr[i]);
-}
-for(int elm:myArr)
-{
-System.out.println(elm);
-}
-}
+  public static void main(String args[])
+  {
+    int[] myArr={100,80,60,40,20};
+    
+    for(int i=0;i<5;i++)
+    {
+      System.out.println(myArr[i]);
+    }
+    for(int elm:myArr)
+    {
+      System.out.println(elm);
+    }
+  }
 }
