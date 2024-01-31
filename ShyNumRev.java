9eb49d1d3f4c79cfcd74219745e6e27e0134ebd1
shaneVR097 <121772441+shaneVR097@users.noreply.github.com>
2024-01-31 23:43:02 +0530

    Update ShyNumRev.java

--- ShyNumRev.java
+++ ShyNumRev.java
@@ -1,20 +1,18 @@
 import java.util.*;
-public class ShyNumRev{
-public static void main(String args[]){
-int scr=49;
-Scanner sc= new Scanner(System.in);
-int n1,n2,r,nr=0,chc,res;
-System.out.println("Enter a number: ");
-n1 = sc.nextInt();
-//n2 = sc.nextInt();
+public class ShyNumRev {
+    public static void main(String args[]) {
+        int scr = 49;
+        Scanner sc = new Scanner(System.in);
+        int n1, n2, r, nr = 0, chc, res;
+        System.out.println("Enter a number: ");
+        n1 = sc.nextInt();
 
-n2=n1;
-while(n2>0)
-{
-r=n2%10;
-n2/=10;
-nr=nr*10+r;
-}
-System.out.println("Reversed number is: "+nr);
-}
+        n2 = n1;
+        while (n2 > 0) {
+            r = n2 % 10;
+            n2 /= 10;
+            nr = nr * 10 + r;
+        }
+        System.out.println("Reversed number is: " + nr);
+    }
 }
