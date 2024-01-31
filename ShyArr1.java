7e933509c3af3982605fd9bbba124033edbaedc4
shaneVR097 <121772441+shaneVR097@users.noreply.github.com>
2024-01-31 21:44:31 +0530

    Update ShyArr1.java

--- ShyArr1.java
+++ ShyArr1.java
@@ -1,15 +1,13 @@
-public class ShyArr1{
-public static void main(String args[]){
-int[] myArr={100,80,60,40,20};
-/*
-int[] myArr=new int[3];
-int[] myArr={1,2,3};
-int[] myArr=new int[]{1,2,3};
-*/
-int idx=0;
-while(idx<5){
-System.out.println(myArr[idx]);
-idx++;
-}
-}
+public class ShyArr1
+{
+          public static void main(String args[])
+          {
+                  int[] myArr={100,80,60,40,20};
+                  int idx=0;
+                  while(idx<5)
+                  {
+                    System.out.println(myArr[idx]);
+                    idx++;
+                  }
+          }
 }
