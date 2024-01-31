0efe9711c920d183c7ea5249f6a7b01eddc06d5b
shaneVR097 <121772441+shaneVR097@users.noreply.github.com>
2024-01-31 23:19:26 +0530

    Update ShyNumDigitsAdd.java

--- ShyNumDigitsAdd.java
+++ ShyNumDigitsAdd.java
@@ -1,21 +1,20 @@
 import java.util.*;
-public class ShyNumDigitsAdd{
-public static void main(String args[]){
-int scr=49;
-Scanner sc= new Scanner(System.in);
-int n1,n2,r,nr=0,chc,res;
-System.out.println("Enter a number: ");
-n1 = sc.nextInt();
-//n2 = sc.nextInt();
+public class ShyNumDigitsAdd {
+    public static void main(String args[]) {
+        int scr = 49;
+        Scanner sc = new Scanner(System.in);
+        int n1, n2, r, nr = 0, chc, res;
 
-n2=n1;
-while(n2>0)
-{
-r=n2%10;
-n2/=10;
-nr=nr+r;
-}
-System.out.println("Sum of digits of the number is: "+nr);
-}
-}
+        System.out.println("Enter a number: ");
+        n1 = sc.nextInt();
 
+        n2 = n1;
+        while (n2 > 0) {
+            r = n2 % 10;
+            n2 /= 10;
+            nr = nr + r;
+        }
+
+        System.out.println("Sum of digits of the number is: " + nr);
+    }
+}
